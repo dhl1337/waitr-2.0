f1afb1a3756333f5c97fc8e9dcf4e9f9c07b3c99
Dan Le <dhl1337@gmail.com>
2016-11-06 20:52:09 -0600

    add user component on server

--- server/user/UserRoute.js
+++ server/user/UserRoute.js
@@ -1,1 +1,9 @@
+var UserController = require('./UserController');
+
+module.exports = function (app) {
+  app.get('/api/user', UserController.read);
+  app.put('/api/user/:id', UserController.update);
+  app.delete('/api/user/:id', UserController.delete);
+  app.get('/api/user/:id', UserController.currentUser);
+};
 
