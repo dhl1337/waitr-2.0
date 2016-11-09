e4a6a979820c053164f6ae3fab9665a98e647695
Dan Le <dhl1337@gmail.com>
2016-11-09 12:47:42 -0600

    change waitlist model and route to es6

--- server/waitlist/WaitlistRoute.js
+++ server/waitlist/WaitlistRoute.js
@@ -1,13 +1,15 @@
-var WaitlistController = require('./WaitlistController');
+import WaitlistController from './WaitlistController';
 
-module.exports = function (app) {
-  app.post('/api/waitlist', WaitlistController.create);
+module.exports = app => {
   app.get('/api/waitlist', WaitlistController.read);
-  app.put('/api/waitlist/:id', WaitlistController.update);
-  app.delete('/api/waitlist/:id', WaitlistController.delete);
-
-  app.put('/api/waitlist/:id/list', WaitlistController.addToList);
-  app.delete('/api/waitlist/:id/list/:listId', WaitlistController.removeFromList);
   app.get('/api/waitlist/:id/list/:listId', WaitlistController.getFromList);
+
+  app.post('/api/waitlist', WaitlistController.create);
+
+  app.put('/api/waitlist/:id', WaitlistController.update);
+  app.put('/api/waitlist/:id/list', WaitlistController.addToList);
   app.put('/api/waitlist/:id/list/:listId', WaitlistController.updateListEntry);
+
+  app.delete('/api/waitlist/:id', WaitlistController.delete);
+  app.delete('/api/waitlist/:id/list/:listId', WaitlistController.removeFromList);
 };
